9612e15e0eddbad3887eb3ce07feafff229d438a
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-07-02 20:34:51 -0300

    feat: add error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,17 +8,29 @@
                 class="w-full border-b bg-transparent px-1 py-2 focus:border-weather-secondary focus:shadow-[0px_1px_0_0_#004e71] focus:outline-none"
                 @input="getSearchResults"
             />
+
             <ul
+                class="absolute top-[66px] w-full bg-weather-secondary px-1 py-2 text-white shadow-md"
                 v-if="mapBoxSearchResults"
-                class="absolute top-[66px] w-full bg-weather-secondary px-2 py-2 text-white shadow-md"
             >
-                <li
-                    v-for="searchResult in mapBoxSearchResults"
-                    :key="searchResult.id"
-                    class="cursor-pointer py-2"
+                <p class="py-2" v-if="searchError">
+                    Sorry, something went wrong, please try again.
+                </p>
+                <p
+                    class="py-2"
+                    v-if="!searchError && mapBoxSearchResults.length === 0"
                 >
-                    {{ searchResult.place_name }}
-                </li>
+                    No results match your query, try a different term.
+                </p>
+                <template v-else>
+                    <li
+                        v-for="searchResult in mapBoxSearchResults"
+                        :key="searchResult.id"
+                        class="cursor-pointer py-2"
+                    >
+                        {{ searchResult.place_name }}
+                    </li>
+                </template>
             </ul>
         </div>
     </main>
@@ -42,6 +54,7 @@ interface MapBoxFeature {
 const searchQuery = ref<string | null>(null)
 const queryTimeout = ref<ReturnType<typeof setTimeout> | null>(null)
 const mapBoxSearchResults = ref<MapBoxFeature[] | null>(null)
+const searchError = ref<boolean>(false)
 
 const getSearchResults = () => {
     if (queryTimeout.value) {
@@ -59,9 +72,8 @@ const getSearchResults = () => {
             } catch (error) {
                 console.error('Error fetching search results:', error)
                 mapBoxSearchResults.value = null
+                searchError.value = true
             }
-        } else {
-            mapBoxSearchResults.value = null
         }
     }, 500)
 }
